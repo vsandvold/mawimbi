9b6affa50037239188d191b2a70e0cd43fd13d76
Vegard Sandvold <vegard.sandvold@schibsted.com>
2020-02-10 22:49:04 +0100

    moved test in to tests folder

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+// TODO: read up on react testing https://create-react-app.dev/docs/running-tests
+
+it('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
