f580fd02f324fd9f213b16707d0b67eae42cf16e
Vegard Sandvold <vegard.sandvold@schibsted.com>
2020-02-10 22:36:00 +0100

    added antd dark theme

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const darkTheme = require('@ant-design/dark-theme');
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: 'css',
-  }),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: darkTheme,
+    }),
 );
