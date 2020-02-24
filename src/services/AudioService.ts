5abd8aed7610310f51ecb88e150891bebb59c3e9
Vegard Sandvold <6734787+vsandvold@users.noreply.github.com>
2020-02-24 17:10:46 +0100

    added basic audio service

---
+++ src/services/AudioService.ts
@@ -0,0 +1,20 @@
+import { AudioContext } from 'standardized-audio-context';
+import StartAudioContext from 'startaudiocontext';
+import Tone from 'tone';
+
+class AudioService {
+
+    static initAudioContext() {
+
+        // Overrides Tone.context with a more updated version from standardized-audio-context
+        const audioContext = new AudioContext();
+        Tone.setContext(audioContext);
+    }
+
+    static startAudioContext(elementSelector: String | null, runningCallback: Function) {
+
+        // Starts the Web Audio API's AudioContext on an explicit user action.
+        // On iOS, the context will be started on the first valid user action on the elementSelector element
+        StartAudioContext(Tone.context, elementSelector, runningCallback);
+    }
+}
