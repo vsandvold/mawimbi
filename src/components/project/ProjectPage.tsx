550d05e1d2d1780ff40db627393010b0006c4fb3
Vegard Sandvold <6734787+vsandvold@users.noreply.github.com>
2020-04-26 03:38:17 +0200

    moved upload callback into project effects hook

--- src/components/project/ProjectPage.tsx
+++ src/components/project/ProjectPage.tsx
@@ -1,11 +1,8 @@
-import { message } from 'antd';
-import React, { useCallback } from 'react';
-import AudioService from '../../services/AudioService';
+import React from 'react';
 import { PageContent, PageHeader, PageLayout } from '../layout/PageLayout';
 import Workstation from '../workstation/Workstation';
 import './ProjectPage.css';
 import ProjectPageHeader from './ProjectPageHeader';
-import { ADD_TRACK } from './projectReducer';
 import { ProjectDispatch } from './useProjectDispatch';
 import useProjectEffects from './useProjectEffects';
 import useProjectReducer from './useProjectReducer';
@@ -15,44 +12,18 @@ const ProjectPage = () => {
 
   const [state, dispatch] = useProjectReducer();
 
-  useProjectEffects(state, dispatch);
+  const { title, tracks } = state;
 
-  const { title } = state;
+  const { uploadFile } = useProjectEffects({}, state, dispatch);
 
-  const uploadFileCallback = useCallback((file: File) => {
-    const messageKey = 'uploadFile';
-    const reader = new FileReader();
-    reader.onabort = () =>
-      message.info({ content: file.name, key: messageKey });
-    reader.onerror = () =>
-      message.error({ content: file.name, key: messageKey });
-    reader.onload = () => {
-      const arrayBuffer = reader.result as ArrayBuffer;
-      AudioService.decodeAudioData(arrayBuffer)
-        .then((audioBuffer) => {
-          dispatch([ADD_TRACK, audioBuffer]);
-          message.success({ content: file.name, key: messageKey });
-        })
-        .catch((error) => {
-          message.error({ content: `${file.name}: ${error}`, key: messageKey });
-        });
-    };
-    message.loading({ content: file.name, key: messageKey });
-    reader.readAsArrayBuffer(file);
-  }, []); // dispatch never changes, and can safely be omitted from dependencies
-
-  // TODO: optimize rendering with React.memo, React.useMemo and React.useCallback
   return (
     <ProjectDispatch.Provider value={dispatch}>
       <PageLayout>
         <PageHeader>
-          <MemoizedProjectPageHeader
-            title={title}
-            uploadFile={uploadFileCallback}
-          />
+          <MemoizedProjectPageHeader title={title} uploadFile={uploadFile} />
         </PageHeader>
         <PageContent>
-          <Workstation tracks={state.tracks} uploadFile={uploadFileCallback} />
+          <Workstation tracks={tracks} uploadFile={uploadFile} />
         </PageContent>
       </PageLayout>
     </ProjectDispatch.Provider>
--- src/components/project/useProjectEffects.tsx
+++ src/components/project/useProjectEffects.tsx
@@ -1,8 +1,41 @@
-import { ProjectAction, ProjectState } from './projectReducer';
+import { message } from 'antd';
+import { useCallback } from 'react';
+import AudioService from '../../services/AudioService';
+import { ADD_TRACK, ProjectAction, ProjectState } from './projectReducer';
 
 const useProjectEffects = (
+  props: any,
   state: ProjectState,
   dispatch: React.Dispatch<ProjectAction>
-) => {};
+) => {
+  /*
+   * Upload and decode audio file.
+   */
+
+  const uploadFile = useCallback((file: File) => {
+    // FIXME: use unique messageKey per file to upload
+    const messageKey = 'uploadFile';
+    const reader = new FileReader();
+    reader.onabort = () =>
+      message.info({ content: file.name, key: messageKey });
+    reader.onerror = () =>
+      message.error({ content: file.name, key: messageKey });
+    reader.onload = () => {
+      const arrayBuffer = reader.result as ArrayBuffer;
+      AudioService.decodeAudioData(arrayBuffer)
+        .then((audioBuffer) => {
+          dispatch([ADD_TRACK, audioBuffer]);
+          message.success({ content: file.name, key: messageKey });
+        })
+        .catch((error) => {
+          message.error({ content: `${file.name}: ${error}`, key: messageKey });
+        });
+    };
+    message.loading({ content: file.name, key: messageKey });
+    reader.readAsArrayBuffer(file);
+  }, []); // dispatch never changes, and can safely be omitted from dependencies
+
+  return { uploadFile };
+};
 
 export default useProjectEffects;
