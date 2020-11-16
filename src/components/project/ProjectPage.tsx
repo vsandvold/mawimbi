2bb6e397e71d57e4daa4c26634ef26899ec2b145
Vegard Sandvold <6734787+vsandvold@users.noreply.github.com>
2020-11-16 19:47:21 +0100

    aadded fullscreen button

--- src/components/project/ProjectPage.tsx
+++ src/components/project/ProjectPage.tsx
@@ -1,29 +1,56 @@
+import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import React from 'react';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 import { PageContent, PageHeader, PageLayout } from '../layout/PageLayout';
 import Workstation from '../workstation/Workstation';
 import './ProjectPage.css';
+import { useUploadFile } from './projectPageEffects';
 import ProjectPageHeader from './ProjectPageHeader';
 import { ProjectDispatch } from './useProjectDispatch';
-import { useUploadFile } from './projectPageEffects';
 import useProjectReducer from './useProjectReducer';
 
 const ProjectPage = () => {
   const [state, dispatch] = useProjectReducer();
 
-  const { title, tracks } = state;
-
   const uploadFile = useUploadFile(dispatch);
+
+  const fullscreenHandle = useFullScreenHandle();
+  const fullScreenButton = (
+    <Button
+      type="link"
+      size="large"
+      className="button"
+      icon={
+        fullscreenHandle.active ? (
+          <FullscreenExitOutlined />
+        ) : (
+          <FullscreenOutlined />
+        )
+      }
+      title={fullscreenHandle.active ? 'Exit fullscreen' : 'Enter fullscreen'}
+      onClick={
+        fullscreenHandle.active ? fullscreenHandle.exit : fullscreenHandle.enter
+      }
+    />
+  );
 
   return (
     <ProjectDispatch.Provider value={dispatch}>
-      <PageLayout>
-        <PageHeader>
-          <MemoizedProjectPageHeader title={title} uploadFile={uploadFile} />
-        </PageHeader>
-        <PageContent>
-          <Workstation tracks={tracks} uploadFile={uploadFile} />
-        </PageContent>
-      </PageLayout>
+      <FullScreen handle={fullscreenHandle}>
+        <PageLayout>
+          <PageHeader>
+            <MemoizedProjectPageHeader
+              title={state.title}
+              uploadFile={uploadFile}
+            />
+          </PageHeader>
+          <PageContent>
+            <Workstation tracks={state.tracks} uploadFile={uploadFile} />
+          </PageContent>
+        </PageLayout>
+        <div className="fullscreen__button">{fullScreenButton}</div>
+      </FullScreen>
     </ProjectDispatch.Provider>
   );
 };
