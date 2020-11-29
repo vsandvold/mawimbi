20196762757d6ef2b58c7509104688223f79eee5
Vegard Sandvold <6734787+vsandvold@users.noreply.github.com>
2020-11-29 16:05:43 +0100

    Bump CRA (#41)
    * upgrade to cra 4
    
    * update lock file

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { BrowserSupportProvider } from './browserSupport';
 import App from './components/App';
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 import AudioService from './services/AudioService';
-import * as serviceWorker from './serviceWorker';
 
 AudioService.startAudio()
   .then(() => console.log('audio is ready'))
@@ -21,7 +21,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
